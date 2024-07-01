d0aafc7c3dd9e556bd715843abaa99f694e006a3
dexterski007 <mbouj007@gmail.com>
2024-07-01 22:22:48 +0100

    fix compilation script

--- compil.sh
+++ compil.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
-
-# Capture the output of sdl2-config commands
-SDL2_CFLAGS=$(sdl2-config --cflags)
-SDL2_LIBS="$(sdl2-config --libs) -lSDL2_ttf"
-
 # Compile the program
-gcc ./src/*.c $SDL2_CFLAGS $SDL2_LIBS -Wall -Wextra -Werror -pedantic -std=c11 -o myapp -lm -lSDL2 -lSDL2_image -lSDL2_ttf
+# shellcheck disable=SC2046
 
+gcc ./src/*.c $(sdl2-config --cflags) $(sdl2-config --libs) -Wall -Wextra -Werror -pedantic -std=c11 -o maze -lm -lSDL2 -lSDL2_image -lSDL2_ttf
